ae196215349e2dc6cdfe9f455b156eba7c0ca1be
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-03-22 23:02:46 +0530

    Added CustomerService layer

---
+++ erp_backend/src/main/java/com/esoft/erp_backend/service/CustomerService.java
@@ -0,0 +1,46 @@
+package com.esoft.erp_backend.service;
+import java.util.*;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esoft.erp_backend.entity.*;
+import com.esoft.erp_backend.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	public CustomerRepository customerRepository;
+	
+	public Customer saveCustomer(Customer customer) {
+		if(customer.getContactPersons()!=null) {
+			for(CustomerDtl dtl :customer.getContactPersons()) {
+				dtl.setCustomer(customer);
+			}
+		}
+		
+		if(customer.getConsignees()!=null) {
+			for(CustomerDtl1 dtl1:customer.getConsignees()) {
+				dtl1.setCustomer(customer);
+			}
+		}
+		
+		return customerRepository.save(customer);
+	}
+	
+	public List<Customer> getAllCustomers(){
+		return customerRepository.findAll();
+	}
+	
+	public Customer getCustomerById(Long id) {
+		Optional<Customer> customer=customerRepository.findById(id);
+		return customer.orElse(null);
+	}
+	public void deleteCustomer(Long id) {
+		customerRepository.deleteById(id);
+	}
+	
+	
+}
