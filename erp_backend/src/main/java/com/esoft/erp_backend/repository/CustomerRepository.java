35ad84035fb8535880679e1a44bbeadbcac0aa60
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-06-30 23:14:15 +0530

    Added findByAroma method in CustomerRepository

--- erp_backend/src/main/java/com/esoft/erp_backend/repository/CustomerRepository.java
+++ erp_backend/src/main/java/com/esoft/erp_backend/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.esoft.erp_backend.repository;
 import com.esoft.erp_backend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 public interface CustomerRepository extends JpaRepository<Customer,Long>{
-
+	List<Customer> findByAroma(String aroma);
 }
