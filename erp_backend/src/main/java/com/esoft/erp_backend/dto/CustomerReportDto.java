b43ff07749573542bffdc7c0c27f292cfcaf3c95
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-06-30 23:06:36 +0530

    Created Customer Report DTO to transfer data between layers

---
+++ erp_backend/src/main/java/com/esoft/erp_backend/dto/CustomerReportDto.java
@@ -0,0 +1,24 @@
+package com.esoft.erp_backend.dto;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CustomerReportDto {
+
+	private String name;
+	private String address1;
+	private String address2;    
+	private String address3;    		
+	private String district;    		
+	private String state;       		
+	private String pin;         		
+	private String phone1;      		
+	private String phone2;      		
+	private String phone3;      		
+	private String email;       		
+	private String web;         		
+	
+}
