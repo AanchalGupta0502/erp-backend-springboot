b08a29211d590b8906f2d92eb633f24e2ecd2d23
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-08-25 23:19:00 +0530

    Made the repository layer for Purchase Order Module

--- erp_backend/src/main/java/com/esoft/erp_backend/repository/PurchaseOrderRepository.java
+++ erp_backend/src/main/java/com/esoft/erp_backend/repository/PurchaseOrderRepository.java
@@ -3,6 +3,7 @@ import com.esoft.erp_backend.entity.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
-public interface PurchaseOrderInterface extends JpaRepository<PurchaseOrder, Long> {
-
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+		Optional<PurchaseOrder> findByPoNo(String po);
+		void deleteByPoNo(String po);
 }
